c436608060c95411423d255ff402a98def590689
Jeremy Jules Flores <jeremyjules.flores@cit.edu>
2022-11-28 22:07:09 +0800

    Update and Add Given

--- AutomataPDA/Program.cs
+++ AutomataPDA/Program.cs
@@ -1,12 +1,54 @@
 ﻿using AutomataPDA.Checker;
+PDAChecker checker;
+//GIVEN STRINGS
 
+//given 1
+var given = "racecar";
+Console.WriteLine("Given: "+given);
+checker = new PalindromeChecker();
+Console.WriteLine("Palindrome: ");
+execute(given, checker);
+checker = new BracketChecker();
+Console.WriteLine("Bracket: ");
+execute(given,checker);
+
+//given 2
+given = "{[]}([])";
+Console.WriteLine("\r\n\r\nGiven: " + given);
+checker = new PalindromeChecker();
+Console.WriteLine("Palindrome: ");
+execute(given, checker);
+checker = new BracketChecker();
+Console.WriteLine("Bracket: ");
+execute(given, checker);
+
+//given 3
+given = "([{}])";
+Console.WriteLine("\r\n\r\nGiven: " + given);
+checker = new PalindromeChecker();
+Console.WriteLine("Palindrome: ");
+execute(given, checker);
+checker = new BracketChecker();
+Console.WriteLine("Bracket: ");
+execute(given, checker);
+
+//given 4
+given = "aabb";
+Console.WriteLine("\r\n\r\nGiven: " + given);
+checker = new PalindromeChecker();
+Console.WriteLine("Palindrome: ");
+execute(given, checker);
+checker = new BracketChecker();
+Console.WriteLine("Bracket: ");
+execute(given, checker);
+
+
+//USER INPUT
+
+Console.WriteLine("\r\n\r\n\r\n----------------\r\nTry It:");
 Console.WriteLine("a.) Palindrome \r\nb.) Brackets");
 Console.WriteLine("Answer: ");
 string? input = Console.ReadLine();
-PDAChecker checker;
-Console.WriteLine("Input String(to check): ");
-string? stringToCheck = Console.ReadLine();
-
 switch (input)
 {
     case "a":
@@ -21,4 +63,13 @@ switch (input)
         throw new ArgumentException();
 }
 
+Console.WriteLine("Input String(to check): ");
+string? stringToCheck = Console.ReadLine();
+
 Console.WriteLine(checker.check(stringToCheck));
+
+
+void execute(string userInput, PDAChecker pdaChecker)
+{
+    Console.WriteLine(pdaChecker.check(userInput));
+}
